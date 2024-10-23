5f9de31e6ae7dfb991c5a44ab5e6e3fd02e17eab
Aditya <aditya1115.ar@gmail.com>
2024-10-23 19:37:29 +0530

    updated config

--- liveblocks.config.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@liveblocks/client";
-import { createRoomContext } from "@liveblocks/react";
-
-const client = createClient({
-  publicApiKey: "pk_prod_giNPugs-BwfJeN_TsRAZF9azg4NM_KtQCHN7NQ-RWtTfZJFWoR5bSX5q1FIBQboO",
-});
-
-export const { RoomProvider, useOthersMapped, useOthers, useMyPresence } = createRoomContext(client);
