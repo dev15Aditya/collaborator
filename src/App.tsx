925d890d09e2277be603badbd01e277714965861
Aditya <aditya1115.ar@gmail.com>
2024-10-23 19:26:03 +0530

    fixing built error

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Footer from "./components/Footer";
 import NavbarComp from "./components/Navbar";
-import WhiteBoard from "./pages/WhiteBoard";
 import Homepage from "./pages/Homepage";
+import WhiteBoard from "./pages/WhiteBoard";
 
 function App() {
   return (
     <Router>
-    <div className="container min-h-[100vh]">
-    <NavbarComp />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/whiteboard/:roomId"
-          element={<WhiteBoard />}
-        />
-      </Routes>
-      <Footer/>
-    </div>
+      <div className="container min-h-[100vh]">
+        <NavbarComp />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/whiteboard/:roomId"
+            element={<WhiteBoard />}
+          />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   )
 }
